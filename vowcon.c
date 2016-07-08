2319207bcab62f268759e4e88d8b57d92e4d2b36
AparnaAbi <aparna8696@gmail.com>
2016-07-08 10:40:30 +0530

    finish

---
+++ vowcon.c
@@ -0,0 +1,12 @@
+# vow
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter the character");
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+printf("the character is a vowel");
+else
+printf("the character is a consonant");
+}
